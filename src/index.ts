516cb4f193b62118fd6de370270a6dc72399d5e8
henceprovedkadam <vedanshookadam55@gmail.com>
2026-02-26 10:19:04 +0530

    feat: validate if the files exist or not before extracting the zip

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,35 @@
 import Admzip from "adm-zip";
+import { existsSync, readdirSync } from "node:fs";
 import { dirname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const extractPath = join(__dirname, "assets", "Extracted");
 const assetPath = join(
   __dirname,
   "assets",
   "tmp601072_2025-06-01_2025-06-30_TAX_INVOICE.zip",
 );
 
-console.log("Asset Path: ", assetPath);
-try {
-  const zip = new Admzip(assetPath);
-  const zipEntries = zip.getEntries();
-  console.log(`Found ${zipEntries.length} items.`);
-  zip.extractAllTo("./src/assets/Extracted");
-} catch (error) {
-  console.error("Error", error);
+function extractZip(assetPath: string) {
+  try {
+    const zip = new Admzip(assetPath);
+    const zipEntries = zip.getEntries();
+    console.log(`Found ${zipEntries.length} items.`);
+    zip.extractAllTo("./src/assets/Extracted");
+  } catch (error) {
+    console.error("Extract Zip Error: ", error);
+  }
+}
+
+if (existsSync(extractPath)) {
+  const files = readdirSync(extractPath);
+  if (files.length > 0) {
+    console.log("Zip file is already extracted.");
+  } else {
+    extractZip(assetPath);
+  }
+} else {
+  extractZip(assetPath);
 }
